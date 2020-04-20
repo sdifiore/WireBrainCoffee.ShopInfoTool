5e3710f72e0520666a3052bb824516b73f3b2ec1
Sergio Di Fiore <difiores@outlook.com>
2020-04-20 17:36:28 -0300

    Add project files.

---
+++ WireBrainCoffee.ShopInfoTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WireBrainCoffee.ShopInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+        }
+    }
+}
