b33ab8130b0a1868abfdbbd6520dc08205cd5c8e
Sergio Di Fiore <difiores@outlook.com>
2020-04-21 13:07:21 -0300

    Add data access project to solution

---
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,9 @@
+﻿namespace WiredBrainCoffee.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+
+        public int BeansInStock { get; set; }
+    }
+}
---
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShopDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WiredBrainCoffee.DataAccess.Model
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStock = 107 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansInStock = 23 };
+            yield return new CoffeeShop { Location = "Munic", BeansInStock = 56 };
+        }
+    }
+}
